ea056334af184554085c69c216945c126b3813dc
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-07-05 20:42:10 -0400

    Update Main.php

--- src/JoinTips/Main.php
+++ src/JoinTips/Main.php
@@ -15,4 +15,17 @@ public function onEnable(){
 $this->getServer->getPluginManager->registerEvents($this, $this);
 $this->getServer()->getLogger->info(TextFormat::BLUE."Join" . TextFormat::GOLD . "Tips" . TextFormat::GREEN . " Enabled!");
 $this->getServer()->getLogger->info(TextFormat::BLUE."Join" . TextFormat::GOLD . "Tips" . TextFormat::GREEN . " Created By " . TextFormat::LIGHT_PURPLE . "ItzBulkDev");
-$this->saveDefaultConfig
+$this->saveDefaultConfig;
+$config = $this->getConfig();
+$tip = $this->getConfig("Player-Tip");
+$tipb = $this->getConfig("Server-Tip");
+}
+
+public function onPlayerJoin(PlayerJoinEvent $event){
+  $p = $event->getPlayer();
+  $pb = $p->getName();
+  $pc = $this->getServer()->getOnlinePlayers();
+  $p->sendTip($tip);
+  $pc->sendTip($tipb);
+}
+}
